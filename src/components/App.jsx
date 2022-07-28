0de7af902040bb3039caf867b460e16eddb8d727
Volodymyr Romanenko <98472440+Mr-Nihility@users.noreply.github.com>
2022-07-28 16:42:55 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Button } from 'components/Button/Button';
+import { Loader } from 'components/Loader/Loader';
 import { Modal } from './Modal/Modal';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
+import { createRequest } from '../api/apirequest';
 //---------------------------------------------------------------------//
+const STATUS = {
+  idle: 'idle',
+  loading: 'loading',
+  error: 'error',
+  success: 'success',
+};
 const App = () => {
   const [image, setImage] = useState('');
   const [query, setQuery] = useState('');
+  const [imageList, setImageList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [status, setStatus] = useState(STATUS.idle);
+  const [totalHits, setTotalHits] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!query && page === 1) {
+      return;
+    }
+
+    createRequest(query, page)
+      .then(res => {
+        const { data } = res;
+        setImageList(prev => [...prev, ...data.hits]);
+        setTotalHits(data.totalHits);
+        setStatus(STATUS.success);
+      })
+      .catch(error => {
+        console.log(error);
+        setError(error.message);
+        setStatus(STATUS.error);
+      });
+  }, [query, page]);
+  const loadMore = () => {
+    setPage(prev => prev + 1);
+  };
 
   const handlerOpenModal = img => {
     setImage(img);
@@ -17,7 +53,10 @@ const App = () => {
   };
 
   const handlerForm = search => {
+    setStatus(STATUS.loading);
+    setImageList([]);
     setQuery(search);
+    setPage(1);
   };
 
   return (
@@ -30,53 +69,26 @@ const App = () => {
       }}
     >
       <Searchbar onSubmit={handlerForm} />
-
-      <ImageGallery query={query} handlerOpenModal={handlerOpenModal} />
-
+      {status === STATUS.loading && <Loader />}
+      {status === STATUS.error && <p>{error}</p>}
+      {status === STATUS.idle && (
+        <p
+          style={{ margin: '100px auto', fontSize: '40px' }}
+        >{`Please, enter the search request`}</p>
+      )}
+      {status === STATUS.success && (
+        <ImageGallery
+          imageList={imageList}
+          handlerOpenModal={handlerOpenModal}
+        />
+      )}
       {image && <Modal image={image} onClose={handlerCloseModal} />}
+      {status === STATUS.success && totalHits > 12 * page && (
+        <Button onClick={loadMore} />
+      )}
       <ToastContainer />
     </div>
   );
 };
 
 export { App };
-
-// export class App extends Component {
-//   state = {
-//     image: '',
-//     query: '',
-//   };
-//   // обробник кліка на картинку
-//   handlerOpenModal = img => {
-//     this.setState({ image: img });
-//   };
-//   //закриття модалки
-//   handlerCloseModal = () => {
-//     this.setState({ image: '' });
-//   };
-//   //дізнатись квери строку
-//   handlerForm = query => {
-//     this.setState({ query });
-//   };
-
-//   render() {
-//     const { query, image } = this.state;
-//     return (
-//       <div
-//         style={{
-//           display: 'grid',
-//           gridTemplateColumns: '1fr0',
-//           gridGap: '16px',
-//           paddingBottom: '24px',
-//         }}
-//       >
-//         <Searchbar onSubmit={this.handlerForm} />
-
-//         <ImageGallery query={query} handlerOpenModal={this.handlerOpenModal} />
-
-//         {image && <Modal image={image} onClose={this.handlerCloseModal} />}
-//         <ToastContainer />
-//       </div>
-//     );
-//   }
-// }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,203 +1,34 @@
-import { Button } from 'components/Button/Button';
-import { Loader } from 'components/Loader/Loader';
-import { useState, useEffect } from 'react';
 import { ImgGallery } from './ImageGallery.styles';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
-import { createRequest } from '../../api/apirequest';
+
 import PropTypes from 'prop-types';
 
-const STATUS = {
-  idle: 'idle',
-  loading: 'loading',
-  error: 'error',
-  success: 'success',
-};
-
-const ImageGallery = ({ handlerOpenModal, query }) => {
-  const [imageList, setImageList] = useState([]);
-  const [page, setPage] = useState(1);
-  const [status, setStatus] = useState(STATUS.idle);
-  const [totalHits, setTotalHits] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (!query) {
-      return;
-    }
-    setStatus(STATUS.loading);
-    setPage(1);
-    setImageList([]);
-
-    createRequest(query)
-      .then(res => {
-        const { data } = res;
-        setImageList(prev => [...prev, ...data.hits]);
-        setTotalHits(data.totalHits);
-        setStatus(STATUS.success);
-        setPage(prev => prev + 1);
-      })
-      .catch(error => {
-        console.log(error);
-        setError(error.message);
-        setStatus(STATUS.error);
-      });
-  }, [query]);
-
-  const loadMore = () => {
-    createRequest(query, page)
-      .then(res => {
-        const { data } = res;
-        setImageList(prev => [...prev, ...data.hits]);
-        setTotalHits(data.totalHits);
-        setStatus(STATUS.success);
-        setPage(prev => prev + 1);
-      })
-      .catch(error => {
-        setError(error.message);
-        setStatus(STATUS.error);
-      });
-  };
-
-  if (status === STATUS.loading) {
-    return <Loader />;
-  }
-  if (status === STATUS.error) {
-    return (
-      <p
-        style={{
-          margin: '100px auto',
-          fontSize: '40px',
-          color: 'red',
-        }}
-      >
-        {`Something went wronge :(`}
-        <br />
-        {error}
-      </p>
-    );
-  }
-  if (!imageList.length) {
-    return (
-      <p
-        style={{
-          margin: '100px auto',
-          fontSize: '40px',
-        }}
-      >{`Please, enter the search request`}</p>
-    );
-  }
-  if (status === STATUS.success) {
-    return (
-      <>
-        <ImgGallery>
-          {imageList.map(({ id, webformatURL, largeImageURL }) => {
-            return (
-              <ImageGalleryItem
-                key={id}
-                smallImg={webformatURL}
-                largeImg={largeImageURL}
-                handlerOpenModal={handlerOpenModal}
-              />
-            );
-          })}
-        </ImgGallery>
-        {totalHits >= 12 * page && <Button onClick={loadMore} />}
-      </>
-    );
-  }
+const ImageGallery = ({ handlerOpenModal, imageList }) => {
+  return (
+    <ImgGallery>
+      {imageList.map(({ id, webformatURL, largeImageURL }) => {
+        return (
+          <ImageGalleryItem
+            key={id}
+            smallImg={webformatURL}
+            largeImg={largeImageURL}
+            handlerOpenModal={handlerOpenModal}
+          />
+        );
+      })}
+    </ImgGallery>
+  );
 };
 
 ImageGallery.propTypes = {
   handlerOpenModal: PropTypes.func.isRequired,
-  query: PropTypes.string.isRequired,
+  imageList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+    })
+  ),
 };
-
-// class ImageGallery extends Component {
-//   static propTypes = {
-//     handlerOpenModal: PropTypes.func.isRequired,
-//     query: PropTypes.string.isRequired,
-//   };
-
-//   state = {
-//     imageList: [],
-//     page: 1,
-//     status: STATUS.idle,
-//     totalHits: null,
-//   };
-//   componentDidUpdate(prevProps, prevState) {
-//     if (prevProps.query !== this.props.query) {
-//       this.setState({ status: STATUS.loading });
-//       createRequest(this.props.query)
-//         .then(res => {
-//           const { data } = res;
-
-//           this.setState(prevState => ({
-//             imageList: [...data.hits],
-//             page: 2,
-//             totalHits: data.totalHits,
-//             status: STATUS.success,
-//           }));
-//         })
-//         .catch(error => {
-//           this.setState({ status: STATUS.error, error });
-//         });
-//     }
-//   }
-//   //обробник кнопки "завантажити ще"
-// loadMore = () => {
-//   createRequest(this.props.query, this.state.page)
-//     .then(res => {
-//       const { hits } = res.data;
-//       this.setState(prevState => ({
-//         imageList: [...prevState.imageList, ...hits],
-//         page: prevState.page + 1,
-//       }));
-//     })
-//     .catch(error => {
-//       this.setState({ status: STATUS.error, error });
-//     });
-// };
-
-//   //рендер
-//   render() {
-//     const { imageList, page, totalHits, status, error } = this.state;
-
-//     if (status === STATUS.loading) {
-//       return <Loader />;
-//     }
-//     if (status === STATUS.error) {
-//       return <p>{error}</p>;
-//     }
-//     if (!imageList.length) {
-//       return (
-//         <p
-//           style={{
-//             margin: '100px auto',
-//             fontSize: '40px',
-//           }}
-//         >{`Please, enter the search request`}</p>
-//       );
-//     }
-//     if (status === STATUS.success) {
-//       return (
-//         <>
-//           <ImgGallery>
-//             {imageList.map(({ id, webformatURL, largeImageURL }) => {
-//               return (
-//                 <ImageGalleryItem
-//                   key={id}
-//                   smallImg={webformatURL}
-//                   largeImg={largeImageURL}
-//                   handlerOpenModal={this.props.handlerOpenModal}
-//                 />
-//               );
-//             })}
-//           </ImgGallery>
-//           {totalHits >= 12 * page && <Button onClick={this.loadMore} />}
-//         </>
-//       );
-//     }
-//   }
-// }
 
 export { ImageGallery };
